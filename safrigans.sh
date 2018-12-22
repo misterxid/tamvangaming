a4823ce4aecd3d4f97dc611a7054024112b1b89a
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-22 09:03:21 +0700

    Create safrigans.sh

---
+++ safrigans.sh
@@ -0,0 +1,306 @@
+
+#!/system/xbin/bash
+# Bersihkan Layar
+#mau ngapain liat liat 😑
+#mau reqod 
+#ijin dulu ma yang punya
+#tolol100%
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+figlet "MR XiD"|lolcat
+
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [  Author :  -------------MR_XiD--------------  ]"|lolcat
+echo "              [===============================================]"|lolcat
+echo "              [  EMAIL  :    safri4687gmail.com@gmail.com     ]"
+echo "              [  TEAM   :    D35TR0Y SQUAD                    ]"
+echo "              [  WA     :    083851312460                     ]"|lolcat
+echo "              [===============================================]"|lolcat
+echo "              [        GUNAKAN TOOLS INI DENGAN BENAR         ]"|lolcat
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [  D35TR0Y SQUAD | JATIM BLACKHAT.              ]"|lolcat
+echo "              |  ELITE MUSLIM CYBER ARMY | PONOROGO CREW      ]"|lolcat
+echo "              [===============================================]"|lolcat
+echo "              [===============================================]"|lolcat
+echo "              [ MA | MR EXPLOR5 | CRUSHER | cyb3r ghost       ]"|lolcat
+echo "              [===============================================]"|lolcat
+
+
+echo  "=============================================================>"|lolcat
+echo  " 01) Crack Facebook"
+
+echo  "=============================================================>"|lolcat
+echo  " 02) D-Tect"
+
+echo  "=============================================================>"|lolcat
+echo  " 03) Top Up Ilegal ML"
+
+echo  "=============================================================>"|lolcat
+
+echo  " 04) Top Up Ilegal FF"
+
+echo  "=============================================================>"|lolcat
+echo  " 05) Webdav"
+
+echo  "=============================================================>"|lolcat
+echo  " 06) Metasploit"
+
+echo  "=============================================================>"|lolcat
+echo  " 07) Pulsa Gratis"
+
+echo  "=============================================================>"|lolcat
+echo  " 08) Ubuntu"
+
+echo  "=============================================================>"|lolcat
+echo  " 09) SANTET ONLINE "
+
+echo  "=============================================================>"|lolcat
+echo  " 10) SQL "
+
+echo  "=============================================================>"|lolcat
+echo  " 11) multi-brute-webdav "
+
+echo  "=============================================================>"|lolcat
+echo  " 12) BUAT LOGIN DI TERMUX "
+
+echo  "=============================================================>"|lolcat
+echo  " 13) PHISING ML 8 BIT"
+
+echo  "=============================================================>"|lolcat
+echo  " 14)  cc fresh Amazon Dan Untung Carding"
+
+echo  "=============================================================>"|lolcat
+echo  " 15) Exit "
+echo  "=============================================================>"|lolcat
+read -p "[SAFRI GANS ea 😅]> " bro;
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+echo  $red" Installing MBF "
+sleep 1
+pkg update -y
+pkg install python2  -y
+pkg install git -y
+pip2 install mechanize
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook
+cd multi-bruteforce-facebook
+echo "GASS MBF DAH SIAP" | lolcat
+sleep 1
+python2 MBF.py
+echo  $red"TERINSTALL TOD "
+exit
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+echo  $red" Installing D-Tect "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+apt-get install python2
+git clone 
+https://github.com/shawarkhanethicalhacker/D-TECT
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+clear
+echo  $red" TOP UP ILEGAL ML "
+sleep 1
+echo  $white " Kode :5448adq35 | Kode 5644864y36 | Kode : 3158art6455 | Kode : 90426dka656 | Kode : 6456745hkw54 | Kode :2642x655843 "
+echo  $red" Masukin Kode Itu Ke Top Up Kalian ^_^"
+exit
+fi
+
+if [ $bro = 4 ] || [ $bro = 04 ]
+then
+clear
+echo  $red" TOP UP ILEGAL FF "
+sleep 1
+echo  $white"  Kode 5458453pr9 |Kode :260446i5m4 |Kode : 9705465d54 | Kode : 3455b623 | Kode : 84238y548 | Kode 862158t3944"
+echo  $red" Masukin Kode Itu Ke Top Up Kalian ^_^"
+exit
+fi
+
+if [ $bro = 5 ] || [ $bro = 05 ]
+then
+clear
+echo  $red" Installing Webdav "
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna 
+requests
+apt install openssl curl
+pkg install libcurl
+mkdir webdav
+cd ~/webdav
+wget https://pastebin.com/raw/HnVyQPtR -O 
+webdav.py
+chmod 777 webdav.py
+cd ~/
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 06 ] || [ $bro = 6 ]
+then
+clear
+echo  $red" Installing Metasploit "
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+wget
+https://raw.githubusercontent.com/verluchie/termux-metasploit/master/install.sh
+chmod 777 install.sh
+sh install.sh
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 07 ] || [ $bro = 7 ]
+then
+clear
+echo  $red" PULSA GRATIS "
+sleep 1
+echo  $white" Inviet Kode :2bk340dk94 | Inviet Kode 154d955b9 |Inviet Kode : 9p54k644as5 | Inviet Kode 844ipps456 | Inviet Kode : 5qdk548239 | Inviet Kode :19ip9405"
+echo  $red" Masukin Kode Itu Ke Rendemm Pulsa Kalian ^_^"
+exit
+fi
+
+if [ $bro = 08 ] || [ $bro = 8 ]
+then
+clear
+echo  $red" Installing Ubuntu "
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone 
+https://github.com/Neo-Oli/termux-ubuntu.git
+cd ~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+echo " Fix network please wait "
+sleep 1
+echo "nameserver 8.8.8.8" 
+>/data/data/com.termux/files/home/termux-ubuntu/ubuntu-fs/etc/$
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 09 ] || [ $bro = 9 ]
+then
+clear
+echo  $red" Installing Youtube DL "
+sleep 1
+apt update && apt upgrade
+apt install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+echo  $red" TERINSTALL KAN SILAHKAN KALAU MAU SANTET MANTAN "
+exit
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+echo  $red" Installing viSQL "
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone 
+https://github.com/blackvkng/viSQL.git
+cd ~/viSQL
+chmod 777 viSQL.py
+cd ~/
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+echo  $red" Installing Multi-Brute-Webdav "
+apt update && apt upgrade
+pkg install toilet
+pkg install sh
+pkg install git
+git clone 
+https://github.com/INYOURS3RV3R/Multi-Brute-Webdav
+cd ~/Multi-Brute-Webdav
+echo  $red" T E R I N S T A L L "
+exit
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ] 
+then
+clear
+echo  $red"loading cok matamu!!! "
+git clone https://github.com/Harisgitama/termux-loginv2fx.git
+cd termux-loginv2fx
+python2 setup.py
+cd $HOME
+python2 useradd.py
+echo  $red" T E R I N S T A L L "
+exit 
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+echo  $red"installing phising mobile jancok 8bit"
+apt install git
+git clone https://github.com/Senitopeng/PhisingGame.git
+cd PhisingGame
+python2 phising.py
+echo  $red"TERINSTALL COK"
+exit
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+echo  $red" Tobat Bro Jangan Carding Trus Kasian Orang Tua Lu cari duit susah buat lu. ee malah elu cari duit sendiri 😅"
+sleep 1"
+echo  $red" Bila Ingin lebih banyak lagi toolnya tunggu tanggal update ini adalah versi ke 3 tanggal 1 januari 2019 update versi ke 4 dan Selanjut nya"
+exit
+fi
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+echo " Thanks To D35TR0Y SQUAD "
+sleep 1
+echo " JATIM BLACKHAT "
+sleep 1
+echo " CEK BARANG BAWAAN ANDA "
+sleep 1
+echo " PARA UKHTY"
+sleep 1
+echo " SAFRI MASIH GANTENG EA 😅 "
+sleep 1
+echo " KU TUNGGU DI PELAMINAN"
+sleep 1
+exit
+fi
